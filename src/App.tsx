ffbea096b6c9be787c68768f7a74f3236d95f07c
vxc333 <you@example.com>
2024-11-22 04:29:32 -0300

    feat: add Pokemon theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { PokemonList } from "./components/pokemon/pokemonList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gradient-to-b from-red-600 via-red-500 to-red-400">
+      <h1 className="text-4xl font-bold mb-8 text-center text-white drop-shadow-lg">
+        Pokédex
+      </h1>
+      <main className="w-full">
+        <PokemonList />
+      </main>
+      <footer className="mt-8 text-center text-sm text-white">
+        Powered by PokéAPI
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
